83e1d8c164dc15e348aff2a08cc53d662bfcdaf6
XenaScriptX <kenjieclark30@gmail.com>
2026-09-19 16:21:54 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://www.astatine.vip/scripts/all_in_one.lua"))({"MTQ3NjE4OTQyODcyMzc0ODkzNQ"})
+loadstring(game:HttpGet("https://test-nine-snowy-58.vercel.app/scripts/all_in_one.lua"))({"MTQ3NjE4OTQyODcyMzc0ODkzNQ"})
